b1f60d1231401b7580e7761d60c7cb2e8f03dea9
DeniseBergmann <denichabepa@gmail.com>
2024-06-24 21:51:01 -0500

    Comienzo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
+  {
+    path:'principal',
+    loadComponent: () => import('./principal/principal.page').then ( m => m.PrincipalPage)
+  },
+  {
+    path: 'lista-productos',
+    loadComponent: () => import('./lista-productos/lista-productos.page').then( m => m.ListaProductosPage)
+  }
 ];
---
+++ src/app/lista-productos/lista-productos.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-lista-productos',
+  templateUrl: './lista-productos.page.html',
+  styleUrls: ['./lista-productos.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton]
+})
+export class ListaProductosPage {
+
+  constructor() { }
+
+}
